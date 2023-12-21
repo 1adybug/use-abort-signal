57354440ed98f273fbcd2adb4028fdde11d42c95
luzixu <lurongv@qq.com>
2023-12-21 13:47:46 +0800

    添加 useAbortableFetch 方法

--- src/index.ts
+++ src/index.ts
@@ -45,4 +45,64 @@ export function useAbortSignal(effect: (signal: AbortSignal) => Promise<void>, c
     }, callbackOrDeps)
 }
 
-export default useAbortSignal
+const originalFetch = fetch
+
+export function useAbortableFetch(effect: (fetch: typeof originalFetch) => Promise<void>, callback: () => void, deps?: DependencyList): void
+export function useAbortableFetch(effect: (fetch: typeof originalFetch) => Promise<void>, deps?: DependencyList): void
+export function useAbortableFetch(effect: (fetch: typeof originalFetch) => Promise<void>, callbackOrDeps?: (() => void) | DependencyList, deps?: DependencyList) {
+    if (callbackOrDeps === undefined) {
+        useEffect(() => {
+            const controller = new AbortController()
+            const fetch: typeof originalFetch = (input, init) => {
+                init ??= {}
+                init.signal = controller.signal
+                return originalFetch(input, init)
+            }
+            effect(fetch).catch(error => {
+                if (!(error instanceof DOMException) || error.name !== "AbortError") {
+                    throw error
+                }
+            })
+            return () => {
+                controller.abort()
+            }
+        })
+        return
+    }
+    if (typeof callbackOrDeps === "function") {
+        useEffect(() => {
+            const controller = new AbortController()
+            const fetch: typeof originalFetch = (input, init) => {
+                init ??= {}
+                init.signal = controller.signal
+                return originalFetch(input, init)
+            }
+            effect(fetch).catch(error => {
+                if (!(error instanceof DOMException) || error.name !== "AbortError") {
+                    throw error
+                }
+            })
+            return () => {
+                controller.abort()
+                callbackOrDeps()
+            }
+        }, deps)
+        return
+    }
+    useEffect(() => {
+        const controller = new AbortController()
+        const fetch: typeof originalFetch = (input, init) => {
+            init ??= {}
+            init.signal = controller.signal
+            return originalFetch(input, init)
+        }
+        effect(fetch).catch(error => {
+            if (!(error instanceof DOMException) || error.name !== "AbortError") {
+                throw error
+            }
+        })
+        return () => {
+            controller.abort()
+        }
+    }, callbackOrDeps)
+}
