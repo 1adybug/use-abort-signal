8d013da265f80447ed3b777c2eaff54a45dbdaea
luzixu <lurongv@qq.com>
2023-12-22 14:09:40 +0800

    修复一些错误

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "father"
 
 export default defineConfig({
-    esm: { output: "./dist/esm" },
-    cjs: { output: "./dist/cjs" }
+    esm: { output: "dist/esm" },
+    cjs: { output: "dist/cjs" },
+    sourcemap: true
 })
